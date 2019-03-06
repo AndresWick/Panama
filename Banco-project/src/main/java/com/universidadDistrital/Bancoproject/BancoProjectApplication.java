eebea3eaf46183951065348e264411595e6750d1
Carlos Andrés <carlos.andres.aguirre.c@gmail.com>
2019-03-05 23:03:03 -0500

    Se modifica conexión con base de datos del proyecto bancos

--- Banco-project/src/main/java/com/universidadDistrital/Bancoproject/BancoProjectApplication.java
+++ Banco-project/src/main/java/com/universidadDistrital/Bancoproject/BancoProjectApplication.java
@@ -3,12 +3,19 @@ package com.universidadDistrital.Bancoproject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.universidadDistrital.Bancoproject.negocio.Usuario;
+import com.universidadDistrital.Bancoproject.util.ODBManager;
+
 @SpringBootApplication
 public class BancoProjectApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		SpringApplication.run(BancoProjectApplication.class, args);
+		ODBManager odbManager = new ODBManager();
+		Usuario usuario = new Usuario();
+		usuario.setPassword("banco");
+		usuario.setUser("Banco");
+		odbManager.conectar(usuario);
 	}
 
 }
-
